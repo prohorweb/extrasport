3c2596ae01d7e7732f83d66c5af805935b936cd3
prohorweb <prohorweb@gmail.com>
2024-03-22 15:27:56 +0300

    Revert "Final edit"
    This reverts commit 3098f08414f2cf1c96adcb09194be44cb21f58c3.

--- localhost/html/wp-content/themes/nevskoe/section/map.php
+++
@@ -1,4 +0,0 @@
-<section class="map-section">
-        <script
-            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad6232b8a6f3742ec3c72501949478395c205ac7aff323038e77b5b66b430e526&amp;amp;width=100%25&amp;amp;height=400&amp;amp;lang=ru_RU&amp;amp;scroll=true"> </script>
-</section>
